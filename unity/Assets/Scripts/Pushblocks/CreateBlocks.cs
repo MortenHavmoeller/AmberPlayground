6545bd38ede2b3823ddd133e99a7034d2993c2ea
MortenHavmoeller <MortenHavmoeller@users.noreply.github.com>
2018-05-12 14:27:49 +0200

    Updated to Unity 2018.1.0f2, added AsyncAwait plugin

---
+++ unity/Assets/Scripts/Pushblocks/CreateBlocks.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBlocks : MonoBehaviour
+{
+	GameObject originalCube;
+
+	private void Start()
+	{
+		for (int x = 0; x < 25; x++)
+		{
+			for (int y = 0; y < 25; y++)
+			{
+
+			}
+		}
+	}
+
+}
